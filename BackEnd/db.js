2df441f37b4e92cd0102bec24544071f0234533c
Riley Ashford <ra7512@mun.ca>
2020-02-21 15:51:46 -0330

    committed

---
+++ BackEnd/db.js
@@ -0,0 +1,23 @@
+//Didnt get a  Database set up but I have included the functions that would be need
+
+//Database Connection would be here
+
+function connectDatabase(){}
+
+//Basic database functions
+
+function addEmailsToDatabase(emailToAdd){
+
+}
+
+function getDataFromDatabase(receiver){
+
+}
+
+function deleteEmailByID(emailID){
+
+}
+
+function archiveEmailByID(emailID){
+
+}
